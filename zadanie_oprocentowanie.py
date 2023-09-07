6efc6938e8b00d1707377a67720a5ed0acdd0811
Wiktoria Stpiczyńska <w.stpiczynska@gmail.com>
2023-09-07 13:58:48 +0200

    utworzylam pytania odnosnie kredytu

---
+++ zadanie_oprocentowanie.py
@@ -0,0 +1,9 @@
+# pytanka
+
+print("\nprogram wyliczajacy wartosc zadluzenia dla kazdego miesiaca \n ")
+
+kwota_kredytu = int(input("ile wynosi calkowita kwota kredytu? \n "))
+
+oprocentowanie = int(input("jakie jest oprocentowanie kredytu? \n "))
+
+stala_rata = int(input("ile wynosi stala rata? \n "))
