71417afc5bac7288b2689685152b8f1c31f186c3
Wiktoria Stpiczyńska <w.stpiczynska@gmail.com>
2023-09-07 14:00:56 +0200

    skopiowalam inflacje kazdego miesiaca z tabeli inflacji

--- zadanie_oprocentowanie.py
+++ zadanie_oprocentowanie.py
@@ -7,3 +7,53 @@ kwota_kredytu = int(input("ile wynosi calkowita kwota kredytu? \n "))
 oprocentowanie = int(input("jakie jest oprocentowanie kredytu? \n "))
 
 stala_rata = int(input("ile wynosi stala rata? \n "))
+
+# tabela inflacji
+
+inflacja_styczen1 = 1.59282448436825
+
+inflacja_luty1 = -0.453509101198007
+
+inflacja_marzec1 = 2.32467171712441
+
+inflacja_kwiecien1 = 1.26125440724877
+
+inflacja_maj1 = 1.78252628571251
+
+inflacja_czerwiec1 = 2.32938454145522
+
+inflacja_lipiec1 = 1.50222984223283
+
+inflacja_sierpien1 = 1.78252628571251
+
+inflacja_wrzesien1 = 2.32884899407637
+
+inflacja_pazdziernik1 = 0.616921348207244
+
+inflacja_listopad1 = 2.35229588637833
+
+inflacja_grudzien1 = 0.337779545187098
+
+inflacja_styczen2 = 1.57703524727525
+
+inflacja_luty2 = -0.292781442607648
+
+inflacja_marzec2 = 2.48619659017508
+
+inflacja_kwiecien2 = 0.267110317834564
+
+inflacja_maj2 = 1.41795267229799
+
+inflacja_czerwiec2 = 1.05424326726375
+
+inflacja_lipiec2 = 1.4805201044812
+
+inflacja_sierpien2 = 1.57703524727525
+
+inflacja_wrzesien2 = -0.0774206903147018
+
+inflacja_pazdziernik2 = 1.16573339872354
+
+inflacja_listopad2 = -0.404186717638335
+
+inflacja_grudzien2 = 1.49970852083123
